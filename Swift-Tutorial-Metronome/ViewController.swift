d0b4bde804dfa5acf044cd1225291234389f56ca
Dave Poirier <dave@idfusion.com>
2018-07-18 10:39:01 -0400

    Initial Commit

---
+++ Swift-Tutorial-Metronome/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-Tutorial-Metronome
+//
+//  Created by Dave Poirier on 2018-07-18.
+//  Copyright © 2018 Soft.Io. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
